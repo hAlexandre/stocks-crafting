8390b5899d0c41fea4f93820109fd04f47f5e901
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-07-02 01:07:22 -0300

    Finishing style (css) and starting routing v1 implementation

---
+++ stocks-crafting-vue/router/stocksRouter.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+let fs = require ('fs');
+
+
+
+module.exports = router;   
+ 
+
+
+    
+
+
+
+
--- stocks-crafting-vue/src/main.js
+++ stocks-crafting-vue/src/main.js
@@ -1,17 +1,52 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../src/css/dashboard.css'
 import $ from 'jquery'
 window.jQuery = window.$ = $;
 
 
+import VueRouter from 'vue-router'
+import Simuladores from './components/Simuladores.vue'
 
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
 Vue.use(BootstrapVue);
+Vue.use(VueRouter);
+
+//let server = `${ConfigClass.getUrlApi().toString()}`;
+
+
+const routes = [
+    {
+      name: 'Simuladores',
+      path: '/Simuladores',
+      component: Simuladores
+    }    
+  ]
+  
+const router = new VueRouter({mode: 'history', routes: routes});
 
 new Vue({
-  
-  render: h => h(App),
+    render: h => h(App),
+    router
 }).$mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
