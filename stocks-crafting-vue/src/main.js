96d00ad9df08ddcbe8a2671e82880d2c9f9da527
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-07-01 23:30:20 -0300

    Started styling

--- stocks-crafting-vue/src/main.js
+++ stocks-crafting-vue/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+window.jQuery = window.$ = $;
+
+
+
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
 
 new Vue({
+  
   render: h => h(App),
 }).$mount('#app')
