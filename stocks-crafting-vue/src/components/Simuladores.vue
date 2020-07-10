05cbabc1e5c8596086dfed13b7ddcd5470913e33
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-07-10 19:11:11 -0300

    Menu adjustment + Simulation settings

--- stocks-crafting-vue/src/components/Simuladores.vue
+++ stocks-crafting-vue/src/components/Simuladores.vue
@@ -9,21 +9,55 @@
           
         </div>
       </div>
-  
-  <h6>Quanto dinheiro você terá após guardar X reais todo mês após determinado tempo?</h6>
-  
-
-
-  <div class="input-group col-md-4">
-    <input type="text" class="form-control" aria-label="Text input with dropdown button">
-    <div class="input-group-append">
-      <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">% ao mês</button>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="javascript:void(0)">% ao mês</a>
-        <a class="dropdown-item" href="javascript:void(0)">% ao ano</a>               
+  <div class="col-md-8">
+    <h6>1 - Quanto dinheiro você terá após guardar um pouquinho todo mês?</h6>
+    <div style="padding-top:5px">
+      <h7> <b>Quanto vai guardar por mês? </b></h7>  
+    </div>  
+    <div class="input-group" >
+      <input id="monthlyContribution" type="text" class="form-control" aria-label="Text input with dropdown button">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary " type="button">reais por mês</button>      
       </div>
     </div>
+
+    <h7> <b>Por quanto tempo? </b></h7>  
+    <div class="input-group ">
+      <input type="text" class="form-control" aria-label="Text input with dropdown button">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary dropdown-toggle" style="padding-left:57px" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{investmentPeriod}}</button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="javascript:void(0)" @click="investmentPeriod='meses'">meses</a>
+          <a class="dropdown-item" href="javascript:void(0)" @click="investmentPeriod='anos'">anos</a>               
+        </div>
+      </div>    
+    </div>
+
+    <h8> <b>Qual a taxa?</b>  CDI em 10 de julho = 0,38% ao mês</h8> 
+    <div class="input-group ">
+      <input type="text" class="form-control" aria-label="Text input with dropdown button">
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary dropdown-toggle" style="padding-left:25px" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{interestTitle}}</button>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="javascript:void(0)" @click="interestTitle='% ao mês'" >% ao mês</a>
+          <a class="dropdown-item" href="javascript:void(0)" @click="interestTitle='% ao ano'">% ao ano</a>               
+        </div>
+      </div>    
+    </div>
+    <div class="float-right" style="padding:5px 0px 0px 0px"> 
+      <button type="submit" class="btn btn-secondary" @click="simulate()">Simular</button>
+    </div>
+
+    <div style="padding-top:38px">
+      <h6>{{this.interestTitle}} {{this.monthlyContribution}}</h6>
+    </div>
+
+
   </div>
+    
+  
+  
+  
   
   
   
@@ -35,7 +69,38 @@
 
 
 export default {
-  name: 'App'
+  name: 'App',
+  data( ){
+    return {
+      simulationTotal: 0,     
+      simulationInterest: 0,
+      simulationInterestType: null,     
+      interestTitle: "% ao mês",
+      investmentPeriod: "anos",
+      monthlyContribution: 0,
+      isSimulating: null
+    }
+  },
+  methods: {
+    simulate() {
+      this.isSimulating = true;
+      this.simulationInterestType = this.interestTitle;
+      this.monthlyContribution = parseFloat(document.getElementById("monthlyContribution")
+        .value.replace(",","."));
+      
+
+
+    }
+  },
+  mounted(){     
+      this.interestTitle = '% ao mês';
+      this.investmentPeriod = 'anos'
+      
+      this.isSimulating = false;
+      this.simulationTotal = 0,     
+      this.simulationInterest = 0,
+      this.simulationInterestType = null
+  }
 }
 
 
