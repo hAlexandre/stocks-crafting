98371d0b35bbf9beda49dfc7f00fe9788d83c7b2
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-08-24 13:27:19 -0300

    Google analytics still not working

--- stocks-crafting-vue/src/components/Simuladores.vue
+++ stocks-crafting-vue/src/components/Simuladores.vue
@@ -1,5 +1,9 @@
 <template>
 <html>
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+
+</head>
 <body>
 
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
