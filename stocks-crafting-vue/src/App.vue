99996da6144391a2059494dce3c4f8811f3ecb6a
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-08-24 12:56:59 -0300

    Google Analytics setup v0.1

--- stocks-crafting-vue/src/App.vue
+++ stocks-crafting-vue/src/App.vue
@@ -1,7 +1,13 @@
 <template>
 <html>
 <head>
-  <title>Amigos do Dinheiro</title>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MHGTJ49');</script>
+<!-- End Google Tag Manager -->
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176075375-2"></script>
   <script>
@@ -10,9 +16,14 @@
     gtag('js', new Date());
     gtag('config', 'UA-176075375-2');
   </script>
+  <title>Amigos do Dinheiro</title>
 
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHGTJ49"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
