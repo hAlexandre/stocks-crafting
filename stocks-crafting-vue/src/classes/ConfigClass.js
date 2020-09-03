b0c2d0037c9f6a41a05e4811f1aa25fe77e57a36
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-09-03 00:19:03 -0300

    Fix DB Link FINALLY READY!!!!!!!

--- stocks-crafting-vue/src/classes/ConfigClass.js
+++ stocks-crafting-vue/src/classes/ConfigClass.js
@@ -2,7 +2,7 @@ export class ConfigClass {
     
 
     static getUrlApi() {
-        return 'ec2-15-228-23-69.sa-east-1.compute.amazonaws.com:3000';
+        return 'http://ec2-15-228-23-69.sa-east-1.compute.amazonaws.com:3000';
         
     }
 }
