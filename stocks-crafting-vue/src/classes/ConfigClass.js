59d3bda59a5b247e2d4a079cfa36ba4c8efd6fe5
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-09-04 11:44:20 -0300

    HTTPS api

--- stocks-crafting-vue/src/classes/ConfigClass.js
+++ stocks-crafting-vue/src/classes/ConfigClass.js
@@ -2,7 +2,7 @@ export class ConfigClass {
     
 
     static getUrlApi() {
-        return 'https://ec2-15-228-23-69.sa-east-1.compute.amazonaws.com:3000';
-        
+        //return 'https://ec2-18-231-162-183.sa-east-1.compute.amazonaws.com';
+        return 'https://api.amigosdodinheiro.com.br';
     }
 }
