34a490cff03866dbca0d90e3f4c5ce26f43b4dce
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-09-04 03:20:46 -0300

    https fix 4

--- stocks-crafting-vue/src/classes/ConfigClass.js
+++ stocks-crafting-vue/src/classes/ConfigClass.js
@@ -2,7 +2,7 @@ export class ConfigClass {
     
 
     static getUrlApi() {
-        return 'https:////ec2-15-228-23-69.sa-east-1.compute.amazonaws.com:3000/';
+        return 'https://ec2-15-228-23-69.sa-east-1.compute.amazonaws.com:3000/';
         
     }
 }
