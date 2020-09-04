85e896bafdc7de1498986850af06c2b1a395bc6c
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-09-04 13:51:33 -0300

    including keys

--- stocks-crafting-vue/api.js
+++ stocks-crafting-vue/api.js
@@ -33,8 +33,8 @@ const fs = require ('fs');
 const path = require('path');
 const https = require ('https');
 const httpsOptions = {
-    cert: fs.readFileSync(path.join(__dirname, 'server', 'ssl' , 'server.crt')),
-    key: fs.readFileSync(path.join(__dirname, 'server', 'ssl', 'server.key'))
+    cert: fs.readFileSync(path.join(__dirname, 'server', 'ssl' , 'certificate.crt')),
+    key: fs.readFileSync(path.join(__dirname, 'server', 'ssl', 'private.key'))
 }
 https.createServer(httpsOptions, api).listen(3000);
 
