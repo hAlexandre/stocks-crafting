ca38329ace905dfe4883c6279b6039386b0f5510
Alexandre Oliveira <alexandreoliveiraa1@hotmail.com>
2020-09-07 15:51:12 -0300

    Fixing menu bug

--- stocks-crafting-vue/src/App.vue
+++ stocks-crafting-vue/src/App.vue
@@ -24,7 +24,7 @@
 
 <div class="container-fluid">
   <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse show">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse hide">
       <div class="sidebar-sticky pt-3">
         <ul class="nav flex-column">
           
